6a8d4ff7be0931c72ab8d84c5b295150ee7ef0a3
l1ve709 <nof4ce1337@gmail.com>
2024-09-13 22:26:58 +0300

    config file

---
+++ config.sql
@@ -0,0 +1,7 @@
+-- Contact us for help: githubsupport@l1ve709.com
+-- config
+
+-- Configuration settings for the project
+-- Example: Setting default currency or thresholds
+DECLARE @defaultCurrency NVARCHAR(10) = 'USD';
+DECLARE @minSalesThreshold DECIMAL(10, 2) = 1000.00;
